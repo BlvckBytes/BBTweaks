859bedab31ad23f4d9dc61e1c8a68fd14c534612
BlvckBytes <blvckbytes@gmail.com>
2026-06-13 22:02:28 +0200

    worldguardflags: introduced hurt-by-heat, which reacts to all player-damage by heat-sources

--- src/main/java/me/blvckbytes/bbtweaks/WorldGuardFlags.java
+++ src/main/java/me/blvckbytes/bbtweaks/WorldGuardFlags.java
@@ -7,27 +7,43 @@ import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import com.sk89q.worldguard.protection.flags.StateFlag;
 import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
 import io.papermc.paper.event.player.PlayerInsertLecternBookEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.damage.DamageType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerTakeLecternBookEvent;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Set;
 
 public class WorldGuardFlags implements Listener {
+
+  private static final Set<DamageType> HURT_BY_HEAT_DAMAGE_TYPES = Set.of(
+    DamageType.HOT_FLOOR, DamageType.IN_FIRE, DamageType.ON_FIRE, DamageType.CAMPFIRE, DamageType.LAVA
+  );
 
   private final StateFlag lecternTakeFlag;
   private final StateFlag lecternInsertFlag;
   private final StateFlag elytraBoostFlag;
   private final StateFlag spawnerChangeFlag;
+  private final StateFlag hurtByHeatFlag;
 
-  public WorldGuardFlags() {
+  private final Plugin plugin;
+
+  public WorldGuardFlags(Plugin plugin) {
     lecternTakeFlag = tryRegisterFlagOrFail("lectern-take");
     lecternInsertFlag = tryRegisterFlagOrFail("lectern-insert");
     elytraBoostFlag = tryRegisterFlagOrFail("elytra-boost");
     spawnerChangeFlag = tryRegisterFlagOrFail("spawner-change");
+    hurtByHeatFlag = tryRegisterFlagOrFail("hurt-by-heat");
+
+    this.plugin = plugin;
   }
 
   private StateFlag tryRegisterFlagOrFail(String name) {
@@ -82,6 +98,27 @@ public class WorldGuardFlags implements Listener {
 
     if (isFlagDeniedForAt(event.getPlayer(), clickedBlock.getLocation(), spawnerChangeFlag))
       event.setCancelled(true);
+  }
+
+  @EventHandler(ignoreCancelled = true)
+  public void onDamage(EntityDamageEvent event) {
+    if (!(event.getEntity() instanceof Player player))
+      return;
+
+    var damageType = event.getDamageSource().getDamageType();
+
+    if (!HURT_BY_HEAT_DAMAGE_TYPES.contains(damageType))
+      return;
+
+    if (!isFlagDeniedForAt(player, player.getLocation(), hurtByHeatFlag))
+      return;
+
+    event.setCancelled(true);
+
+    // Also clear fire-ticks immediately after leaving the lava/fire/etc.
+    Bukkit.getScheduler().runTaskLater(plugin, () -> {
+      player.setFireTicks(0);
+    }, 1L);
   }
 
   private boolean isFlagDeniedForAt(Player player, Location location, StateFlag flag) {
