2edea8ecc660abfa9a05ba1cf35dc69967d6eadd
BlvckBytes <blvckbytes@gmail.com>
2026-07-12 16:04:56 +0200

    magnet: increased priority of ItemSpawnEvent handler to HIGHEST, as to allow other handlers to modify the stack before we collect it

--- src/main/java/me/blvckbytes/bbtweaks/mechanic/magnet/MagnetMechanic.java
+++ src/main/java/me/blvckbytes/bbtweaks/mechanic/magnet/MagnetMechanic.java
@@ -20,6 +20,7 @@ import org.bukkit.block.sign.Side;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -439,7 +440,7 @@ public class MagnetMechanic extends PredicateMechanic<MagnetInstance> implements
     return true;
   }
 
-  @EventHandler(ignoreCancelled = true)
+  @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
   public void onItemSpawn(ItemSpawnEvent event) {
     var itemEntity = event.getEntity();
 
