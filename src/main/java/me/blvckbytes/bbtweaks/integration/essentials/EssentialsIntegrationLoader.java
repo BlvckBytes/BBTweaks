a2a5ee0e09fb816908717112f7c8b50a1b28c373
BlvckBytes <blvckbytes@gmail.com>
2026-08-16 03:04:16 +0200

    now making the raw plugin-instance available in the essentials integration-loader as to make polymorphism work when wiring

--- src/main/java/me/blvckbytes/bbtweaks/integration/essentials/EssentialsIntegrationLoader.java
+++ src/main/java/me/blvckbytes/bbtweaks/integration/essentials/EssentialsIntegrationLoader.java
@@ -1,16 +1,16 @@
 package me.blvckbytes.bbtweaks.integration.essentials;
 
-import com.earth2me.essentials.IEssentials;
+import com.earth2me.essentials.Essentials;
 import me.blvckbytes.bbtweaks.auto_wirer.WrappedDependency;
 import org.bukkit.Bukkit;
 
 public class EssentialsIntegrationLoader {
 
   @WrappedDependency
-  public final IEssentials essentials;
+  public final Essentials essentials;
 
   public EssentialsIntegrationLoader() {
-    this.essentials = (IEssentials) Bukkit.getPluginManager().getPlugin("Essentials");
+    this.essentials = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
 
     if (essentials == null)
       throw new IllegalStateException("Expected Essentials to be loaded");
