205fe0140e65b6cd8e13f96df2ec3721055d86ce
BlvckBytes <blvckbytes@gmail.com>
2026-08-23 05:00:41 +0200

    locateentities: added total counts as well as teleport functionality

--- src/main/java/me/blvckbytes/bbtweaks/locate_entities/EntityScanSession.java
+++ src/main/java/me/blvckbytes/bbtweaks/locate_entities/EntityScanSession.java
@@ -2,32 +2,42 @@ package me.blvckbytes.bbtweaks.locate_entities;
 
 import it.unimi.dsi.fastutil.longs.Long2IntOpenHashMap;
 import me.blvckbytes.bbtweaks.mechanic.util.IntTuple;
+import me.blvckbytes.bbtweaks.util.MutableInt;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.EnumMap;
 
 public class EntityScanSession {
 
   private static final int MILLISECONDS_PER_TICK = 5;
 
   public final World world;
-  public final EntityType entityType;
+  public final @Nullable EntityType targetEntityType;
 
   private final Chunk[] loadedChunks;
 
-  public final Long2IntOpenHashMap countByChunkTuple;
+  public final @Nullable Long2IntOpenHashMap countByChunkTuple;
+  public final @Nullable EnumMap<EntityType, MutableInt> totalCountByType;
 
   private int nextChunkIndex;
 
-  public EntityScanSession(World world, EntityType entityType) {
+  public EntityScanSession(World world, @Nullable EntityType targetEntityType) {
     this.world = world;
-    this.entityType = entityType;
+    this.targetEntityType = targetEntityType;
 
     this.loadedChunks = world.getLoadedChunks();
-    this.countByChunkTuple = new Long2IntOpenHashMap();
-    this.countByChunkTuple.defaultReturnValue(0);
+
+    this.countByChunkTuple = targetEntityType != null ? new Long2IntOpenHashMap() : null;
+
+    if (this.countByChunkTuple != null)
+      this.countByChunkTuple.defaultReturnValue(0);
+
+    this.totalCountByType = targetEntityType == null ? new EnumMap<>(EntityType.class) : null;
   }
 
   public void run(Plugin plugin, Runnable completionHandler) {
@@ -42,10 +52,15 @@ public class EntityScanSession {
       var chunkTuple = IntTuple.create(chunk.getX(), chunk.getZ());
 
       for (var entity : chunk.getEntities()) {
-        if (entity.getType() != entityType)
-          continue;
+        if (countByChunkTuple != null) {
+          if (entity.getType() == targetEntityType)
+            countByChunkTuple.addTo(chunkTuple, 1);
 
-        countByChunkTuple.addTo(chunkTuple, 1);
+          continue;
+        }
+
+        if (totalCountByType != null)
+          totalCountByType.computeIfAbsent(entity.getType(), _ -> new MutableInt()).value++;
       }
 
       if (System.nanoTime() >= deadline)
--- src/main/java/me/blvckbytes/bbtweaks/locate_entities/LocateEntitiesCommand.java
+++ src/main/java/me/blvckbytes/bbtweaks/locate_entities/LocateEntitiesCommand.java
@@ -3,6 +3,11 @@ package me.blvckbytes.bbtweaks.locate_entities;
 import at.blvckbytes.cm_mapper.section.command.CommandSection;
 import me.blvckbytes.bbtweaks.auto_wirer.CommandHandler;
 import me.blvckbytes.bbtweaks.mechanic.util.IntTuple;
+import me.blvckbytes.bbtweaks.util.TeleportUtil;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
@@ -16,8 +21,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 public class LocateEntitiesCommand implements CommandHandler {
+
+  private static final String COUNTS_SENTINEL = "counts";
+  private static final long MAX_RESULT_LINES = 15;
 
   private final PluginCommand command;
 
@@ -54,24 +63,28 @@ public class LocateEntitiesCommand implements CommandHandler {
     }
 
     if (args.length != 2) {
-      sender.sendMessage("§cUsage: /" + label + " <entity-type> <world>");
+      sender.sendMessage("§cUsage: /" + label + " <world> <entity-type|" + COUNTS_SENTINEL + ">");
       return true;
     }
 
-    EntityType type;
-
-    try {
-      type = EntityType.valueOf(args[0].toUpperCase());
-    } catch (Throwable e) {
-      sender.sendMessage("§cUnknown entity-type: §4" + args[0]);
-      return true;
-    }
-
-    var world = Bukkit.getWorld(args[1]);
+    var world = Bukkit.getWorld(args[0]);
 
     if (world == null) {
-      sender.sendMessage("§cUnknown world: §4" + args[1]);
+      sender.sendMessage("§cUnknown world: §4" + args[0]);
       return true;
+    }
+
+    EntityType type = null;
+
+    var scanForCounts = args[1].equalsIgnoreCase(COUNTS_SENTINEL);
+
+    if (!scanForCounts) {
+      try {
+        type = EntityType.valueOf(args[1].toUpperCase());
+      } catch (Throwable e) {
+        sender.sendMessage("§cUnknown entity-type: §4" + args[1]);
+        return true;
+      }
     }
 
     this.scanSession = new EntityScanSession(world, type);
@@ -80,7 +93,12 @@ public class LocateEntitiesCommand implements CommandHandler {
       var session = this.scanSession;
       this.scanSession = null;
 
-      displayResults(session, sender);
+      if (scanForCounts) {
+        displayTotalCounts(session, sender);
+        return;
+      }
+
+      displayClusters(session, sender);
     });
 
     return true;
@@ -92,44 +110,96 @@ public class LocateEntitiesCommand implements CommandHandler {
       return List.of();
 
     if (args.length == 1) {
-      return Arrays.stream(EntityType.values())
-        .map(Enum::name)
+      return Bukkit.getWorlds().stream()
+        .map(World::getName)
         .filter(name -> StringUtils.startsWithIgnoreCase(name, args[0]))
-        .limit(10)
         .toList();
     }
 
     if (args.length == 2) {
-      return Bukkit.getWorlds().stream()
-        .map(World::getName)
+      return Stream.concat(
+          Arrays.stream(EntityType.values())
+            .map(Enum::name),
+          Stream.of(COUNTS_SENTINEL)
+        )
         .filter(name -> StringUtils.startsWithIgnoreCase(name, args[1]))
+        .limit(10)
         .toList();
     }
 
     return List.of();
   }
 
-  private void displayResults(EntityScanSession scanSession, CommandSender sender) {
-    var clusters = ChunkCluster.findClusters(scanSession.countByChunkTuple);
+  private void displayTotalCounts(EntityScanSession scanSession, CommandSender sender) {
+    if (scanSession.totalCountByType == null)
+      throw new IllegalStateException("Cannot display total counts without the corresponding map being present");
 
-    if (clusters.isEmpty()) {
-      sender.sendMessage("§cNo entities of type §4" + scanSession.entityType.name() + " §care currently loaded in §4" + scanSession.world.getName());
+    if (scanSession.totalCountByType.isEmpty()) {
+      sender.sendMessage("§cThere are no entities currently loaded in §4\"" + scanSession.world.getName() + "\"");
       return;
     }
 
-    sender.sendMessage("§aChunk-Cluster for type " + scanSession.entityType.name() + " of world " + scanSession.world.getName() + ":");
+    record TypeAndCount(EntityType type, int count) {}
+
+    var counts = new ArrayList<TypeAndCount>();
+
+    for (var entry : scanSession.totalCountByType.entrySet())
+      counts.add(new TypeAndCount(entry.getKey(), entry.getValue().value));
+
+    counts.sort((a, b) -> -Integer.compare(a.count, b.count));
+
+    sender.sendMessage("§aTotal entity-counts of world \"" + scanSession.world.getName() + "\":");
+
+    for (var index = 0; index < counts.size(); ++index) {
+      if (index == MAX_RESULT_LINES) {
+        sender.sendMessage("§eStopped printing after 10 lines, had " + (counts.size() - index) + " more");
+        break;
+      }
+
+      var typeAndCount = counts.get(index);
+
+      sender.sendMessage("§a- " + typeAndCount.count + "x " + typeAndCount.type.name());
+    }
+  }
+
+  private void displayClusters(EntityScanSession scanSession, CommandSender sender) {
+    if (scanSession.countByChunkTuple == null || scanSession.targetEntityType == null)
+      throw new IllegalStateException("Cannot display clusters without per-chunk counts being present");
+
+    var clusters = ChunkCluster.findClusters(scanSession.countByChunkTuple);
+
+    if (clusters.isEmpty()) {
+      sender.sendMessage("§cNo entities of type §4" + scanSession.targetEntityType.name() + " §care currently loaded in world \"§4" + scanSession.world.getName() + "\"");
+      return;
+    }
+
+    sender.sendMessage("§aChunk-Cluster for type " + scanSession.targetEntityType.name() + " of world \"" + scanSession.world.getName() + "\":");
 
     for (var index = 0; index < clusters.size(); ++index) {
-      if (index == 10) {
+      if (index == MAX_RESULT_LINES) {
         sender.sendMessage("§eStopped printing after 10 lines, had " + (clusters.size() - index) + " more");
         break;
       }
 
       var cluster = clusters.get(index);
-      var x = IntTuple.getFirst(cluster.highestCountChunk());
-      var z = IntTuple.getSecond(cluster.highestCountChunk());
+      var x = IntTuple.getFirst(cluster.highestCountChunk()) * 16;
+      var z = IntTuple.getSecond(cluster.highestCountChunk()) * 16;
 
-      sender.sendMessage("§a- x=" + x*16 + " z=" + z*16 + " (" + cluster.chunks().size() + " chunks): " + cluster.totalCount() + "x " + scanSession.entityType.name());
+      var safeTarget = TeleportUtil.findSafeTeleportLocation(scanSession.world, x, z);
+
+      var y = safeTarget == null ? 50 : safeTarget.getBlockY();
+
+      sender.sendMessage(
+        Component.text("- x=" + x + " z=" + z + " (" + cluster.chunks().size() + " chunks): " + cluster.totalCount() + "x " + scanSession.targetEntityType.name())
+          .color(NamedTextColor.GREEN)
+          .hoverEvent(
+            HoverEvent.showText(
+              Component.text("Click to teleport to chunk")
+                .color(NamedTextColor.GREEN)
+            )
+          )
+          .clickEvent(ClickEvent.runCommand("tppos " + x + " " + y + " " + z))
+      );
     }
   }
 }
